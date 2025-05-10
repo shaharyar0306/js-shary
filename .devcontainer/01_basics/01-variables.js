01df76e1790b4e45127eec74bbf628a6b3d2d472
shaharyar0306 <shaharyarr05@gmail.com>
2025-05-10 23:55:29 +0000

    admit it

---
+++ .devcontainer/01_basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountID = 3061567
+let accountEmail = "rajpootshary@gmail.com"
+var accountPassword = "34683737"
+accountcity = "lahore"
+
+let accountcity;
+
+// accountid = 2 is not allowed because in const we cant rewrite the vale//
+// prefer not to use var bacause issue in block scope and functional scope//
+
+console.table([accountID,accountEmail,accountPassword,accountcity])
+
+
